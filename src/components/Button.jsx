f9ed38410982a2aed0bcc9cf71e81a93ca097070
Diego Millan <diegomillandev@gmail.com>
2023-09-18 00:31:51 -0500

    update alt button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-const Button = ({label, iconURL}) => {
+const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border text-lg leading-none font-montserrat bg-coral-red rounded-full text-white border-coral-red">
-        {label}
-        <img src={iconURL} alt="" />
+      {label}
+      <img src={iconURL} alt="button icon" />
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
