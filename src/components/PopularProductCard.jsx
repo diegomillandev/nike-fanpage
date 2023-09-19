05c7aa019eace0644ad3e83fd387393d21e8aab4
Diego Millan <diegomillandev@gmail.com>
2023-09-19 01:00:30 -0500

    add animation hover popular products

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -3,13 +3,26 @@ import { star } from "../assets/icons";
 const PopularProductCard = ({ imgURL, name, price }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
-      <img src={imgURL} alt="" className="w-[17.5rem] h-[17.5rem]" />
+      <div className="overflow-hidden w-[17.5rem] h-[17.5rem] relative after:bg-red-300 group rounded-3xl cursor-pointer">
+        <div className="absolute w-full h-full bg-black/20 flex justify-center items-center -bottom-[100%] group-hover:bottom-0 transition-all duration-300">
+          <button className="bg-coral-red text-white font-montserrat font-medium text-xl py-2 px-4 rounded-full hover:scale-105">
+            Add to Cart
+          </button>
+        </div>
+        <img src={imgURL} alt="" className="" />
+      </div>
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="ratio icon" width={24} height={24} />
-        <p className="text-slate-gray leading-normal font-montserrat text-xl">(4.5)</p>
+        <p className="text-slate-gray leading-normal font-montserrat text-xl">
+          (4.5)
+        </p>
       </div>
-      <h3 className="mt-2 text-2xl font-bold leading-normal font-palanquin">{name}</h3>
-      <p className="mt-2 text-xl font-montserrat text-coral-red font-semibold">{price}</p>
+      <h3 className="mt-2 text-2xl font-bold leading-normal font-palanquin">
+        {name}
+      </h3>
+      <p className="mt-2 text-xl font-montserrat text-coral-red font-semibold">
+        {price}
+      </p>
     </div>
   );
 };
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -14,7 +14,7 @@ const PopularProducts = () => {
         </p>
       </div>
 
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-col-2 grid-col-1">
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-col-1">
         {products.map((product) => (
           <PopularProductCard key={product.name} {...product} />
         ))}
