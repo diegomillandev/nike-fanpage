5498d81ed8a8c98d83e382d77322f04f5529424c
Diego Millan <diegomillandev@gmail.com>
2023-09-19 01:14:19 -0500

    update hover hero

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,4 +1,5 @@
 const ShoeCard = ({ imgURL, bigShoeImg, changeBigShoeImage }) => {
+
   const handleClick = () => {
     if (bigShoeImg !== imgURL.bigShoe) {
       changeBigShoeImage(imgURL.bigShoe);
@@ -7,12 +8,12 @@ const ShoeCard = ({ imgURL, bigShoeImg, changeBigShoeImage }) => {
 
   return (
     <div
-      className={`border-2 rounded-xl ${
+      className={`shoe-card border-2 rounded-xl ${
         bigShoeImg === imgURL.bigShoe
           ? "border-coral-red"
           : "border-transparent"
-      } cursor-pointer max-sm:flex-1`}
-      onClick={handleClick}
+      } cursor-pointer max-sm:flex-1 transition-all duration-150`}
+      onMouseEnter={handleClick}
     >
       <div className="flex justify-center items-center bg-card bg-center bg-cover w-30 h-32 sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
         <img
